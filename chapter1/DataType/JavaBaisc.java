6b881d5ef2f5fd220bf68d0440c281af9fdb70fe
ayush-989 <ayusht9189@gmail.com>
2026-04-16 02:23:25 +0530

    Variable and DataType

---
+++ chapter1/DataType/JavaBaisc.java
@@ -0,0 +1,21 @@
+public class JavaBaisc {
+    public static void main(String[] args) {
+          byte a= 8;
+            System.out.println("a="+a);
+          short b = 8;
+           System.out.println("b="+b);
+          
+         boolean var = true ;
+        
+         char c = 'A';
+         System.out.println("c="+c);
+
+         //float price = 0.23 ;
+         //System.out.println("price="+price);
+          int number = 25 ;
+          System.out.print("number"+number);
+
+
+
+    }
+}
